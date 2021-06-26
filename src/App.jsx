d096b46e3c21da8df70b2e98b5b08f27be6ea5e4
Ray Louis <x1337c@gmail.com>
2021-06-26 15:25:04 -0700

    GOT TAILWIND WORKING BY SIMPLY RESTARTING THE VITE SERVER I'M A FUCKING IDIOT

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p className="text-blue-600">Hello Vite + React!</p>
+        <p className="text-blue-400">Hello Vite + React!</p>
         <p>
           <button type="button" onClick={() => setCount((count) => count + 1)}>
             count is: {count}
