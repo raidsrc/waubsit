4fbcb6e19c0cfad8897c155a4bfc464c39bd6a92
Ray Louis <x1337c@gmail.com>
2021-06-30 14:33:47 -0700

    tiny changes
    credited iconmonstr

--- src/Pages.jsx
+++ src/Pages.jsx
@@ -158,19 +158,19 @@ function FindMePage(props) {
             <span className="text-5xl bg-red-600 mb-6">
                site construction in progress BE PATIENT
             </span>
-            <div className="grid grid-cols-3 grid-rows-4">
+            <div className="grid grid-cols-3 grid-rows-6">
                <div className="row-start-2 col-start-2 flex flex-row items-center justify-center">
                   <a target="_blank" rel="noreferrer noopener" href="https://twitter.com/raidsrc"><svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z" /></svg></a>
                   <span className="px-4 text-lg font-semibold">@raidsrc</span>
                </div>
                <div className="row-start-4 col-start-2 flex flex-row items-center justify-center">
-               <a target="_blank" rel="noreferrer noopener" href="https://youtube.com/c/raidsrc"><svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24"><path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z" /></svg></a>
+                  <a target="_blank" rel="noreferrer noopener" href="https://youtube.com/c/raidsrc"><svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24"><path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z" /></svg></a>
                   <span className="px-4 text-lg font-semibold">/c/raidsrc</span>
                </div>
+               <div className="row-start-6 col-start-2 mt-4 text-center text-xs font-semibold">
+                  icons © 2021 iconmonstr
+               </div>
             </div>
-            {
-               // TODO: CREDIT ICONMONSTR FOR THE ICONS
-            }
          </CenteredFullPageFlexContainer>
       </div>
    )
