872a2004cb35f3ec6e5abfc70371840f76cfe105
Ray Louis <x1337c@gmail.com>
2021-07-07 10:13:37 -0700

    this shit broken
    smh

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from 'react'
+import React, { useState, useContextjj, useContext } from 'react'
 import { HomePage, AboutPage, ResumePage, FindMePage } from './Pages'
-import { useTransition } from '@react-spring/core'
+import { useTransition, animated } from 'react-spring'
 import { Navbar } from './ReusableComponents'
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import { BrowserRouter as Router, Route, Switch, useLocation } from "react-router-dom"
+import { __RouterContext } from 'react-router'
 import './App.css'
 
 function App() {
@@ -10,28 +11,42 @@ function App() {
   const [showMount, setShowMount] = useState(true)
   const [arrayOfAnimatedShit, setArrayOfAnimatedShit] = useState([])
 
+  //const {location} = useContext(__RouterContext)
+  let location = useLocation()
+  console.log(location)
+  const transitions = useTransition(location, {
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+    keys: loc => loc.pathname,
+  })
+
   return (
-    <Router>
-      <div className="whole-app">
-        <Navbar />
-        <div className="content">
-          <Switch>
-            <Route exact path="/">
-              <HomePage page={page}/>
-            </Route>
-            <Route exact path="/about">
-              <AboutPage />
-            </Route>
-            <Route exact path="/resume">
-              <ResumePage />
-            </Route>
-            <Route exact path="/contact">
-              <FindMePage />
-            </Route>
-          </Switch>
-        </div>
+    <div className="whole-app">
+      <Navbar />
+      <div className="content">
+        {transitions(
+          ({item, props, key}) => { item &&
+          <animated.div key={key} style={props}>
+            
+          </animated.div>
+        })}
+        <Switch>
+          <Route exact path="/">
+            <HomePage page={page} />
+          </Route>
+          <Route exact path="/about">
+            <AboutPage />
+          </Route>
+          <Route exact path="/resume">
+            <ResumePage page={page} showMount={showMount} setShowMount={setShowMount} />
+          </Route>
+          <Route exact path="/contact">
+            <FindMePage />
+          </Route>
+        </Switch>
       </div>
-    </Router>
+    </div>
   )
 }
 
