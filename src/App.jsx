90d5f3e888a7d78d2fec386ff844ac2a10a87a8c
Ray Louis <x1337c@gmail.com>
2021-07-07 10:48:48 -0700

    react-transition-group is in
    tons less confusing than react-spring holy fuck

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import React, { useState, useContextjj, useContext } from 'react'
 import { HomePage, AboutPage, ResumePage, FindMePage } from './Pages'
 import { Navbar } from './ReusableComponents'
 import { BrowserRouter as Router, Route, Switch, useLocation } from "react-router-dom"
-import { __RouterContext } from 'react-router'
+import { TransitionGroup, CSSTransition } from "react-transition-group"
 import './App.css'
+import "./animationStyles.css"
 
 function App() {
 
-  //const {location} = useContext(__RouterContext)
   let location = useLocation()
   console.log(location)
 
@@ -15,20 +15,26 @@ function App() {
     <div className="whole-app">
       <Navbar />
       <div className="content">
-        <Switch>
-          <Route exact path="/">
-            <HomePage page={page} />
-          </Route>
-          <Route exact path="/about">
-            <AboutPage />
-          </Route>
-          <Route exact path="/resume">
-            <ResumePage page={page} showMount={showMount} setShowMount={setShowMount} />
-          </Route>
-          <Route exact path="/contact">
-            <FindMePage />
-          </Route>
-        </Switch>
+        <div className="h-20"></div>
+        <TransitionGroup>
+          <CSSTransition key={location.key} classNames="fade" timeout={10}>
+
+            <Switch location={location}>
+              <Route exact path="/">
+                <HomePage />
+              </Route>
+              <Route exact path="/about">
+                <AboutPage />
+              </Route>
+              <Route exact path="/resume">
+                <ResumePage />
+              </Route>
+              <Route exact path="/contact">
+                <FindMePage />
+              </Route>
+            </Switch>
+          </CSSTransition>
+        </TransitionGroup>
       </div>
     </div>
   )
