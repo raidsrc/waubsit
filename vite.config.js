b35403030b5d5b0e2816bab8fdcf4e446acac6c2
Ray Louis <x1337c@gmail.com>
2021-07-10 14:26:32 -0700

    got rid of unnecessary shit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
-import shimReactPdf from "vite-plugin-shim-react-pdf"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh(), shimReactPdf()],
+  plugins: [reactRefresh()],
 })
