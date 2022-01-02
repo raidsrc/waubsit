14044aa180c11721d0ac59e390276730e7f40a6c
Ray Louis <x1337c@gmail.com>
2022-01-02 00:05:46 -0800

    i fucking did it.

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ import ResumePage from './Pages/ResumePage'
 import ContactPage from './Pages/ContactPage'
 import LinksPage from './Pages/LinksPage'
 import SiteInfoPage from './Pages/SiteInfoPage.jsx'
-import SupPage from './Pages/SupPage'
 
 function App() {
   let location = useLocation()
@@ -18,42 +17,67 @@ function App() {
     setTimeout(() => alert("i'm not done constructing the website yet. please excuse the presence of any wack shit"), 420)
     setAlerted(true)
   }
+  const routes = [
+    { path: '/home', name: 'Home', Component: HomePage },
+    { path: '/about', name: 'About', Component: AboutPage },
+    { path: '/resume', name: 'Resume', Component: ResumePage },
+    { path: '/contact', name: 'Contact', Component: ContactPage },
+    { path: '/links', name: 'More Stuff', Component: LinksPage },
+    { path: '/siteinfo', name: 'Site Info', Component: SiteInfoPage },
+  ]
 
   return (
     <div className="whole-app">
       <Navbar />
       <div className="content">
-        <TransitionGroup>
-          <CSSTransition key={location.key} classNames="fade" timeout={1000}>
-            {
-              //might need to make this timeout prop above more accurate to the lengths of the css transitions in animationStyles.css
-            }
-            <Switch location={location}>
-              <Route path="/home">
-                <HomePage />
-              </Route>
-              <Route exact path="/about">
-                <AboutPage />
-              </Route>
-              <Route exact path="/resume">
-                <ResumePage />
-              </Route>
-              <Route exact path="/contact">
-                <ContactPage />
-              </Route>
-              <Route exact path="/links">
-                <LinksPage />
-              </Route>
-              <Route exact path="/siteinfo">
-                <SiteInfoPage />
-              </Route>
-              <Route exact path="/sup">
-                <SupPage />
-              </Route>
-              <Redirect exact from="/" to="/home" />
-            </Switch>
-          </CSSTransition>
-        </TransitionGroup>
+        <Route exact path="/home">
+          {({ match }) => (
+            <CSSTransition in={match != null} classNames="fade" unmountOnExit timeout={1000}>
+              <HomePage />
+            </CSSTransition>
+          )}
+        </Route>
+
+        <Route exact path="/about">
+          {({ match }) => (
+            <CSSTransition in={match != null} classNames="fade" unmountOnExit timeout={1000}>
+              <AboutPage />
+            </CSSTransition>
+          )}
+        </Route>
+
+        <Route exact path="/resume">
+          {({ match }) => (
+            <CSSTransition in={match != null} classNames="fade" unmountOnExit timeout={1000}>
+              <ResumePage />
+            </CSSTransition>
+          )}
+        </Route>
+
+        <Route exact path="/contact">
+          {({ match }) => (
+            <CSSTransition in={match != null} classNames="fade" unmountOnExit timeout={1000}>
+              <ContactPage />
+            </CSSTransition>
+          )}
+        </Route>
+
+        <Route exact path="/links">
+          {({ match }) => (
+            <CSSTransition in={match != null} classNames="fade" unmountOnExit timeout={1000}>
+              <LinksPage />
+            </CSSTransition>
+          )}
+        </Route>
+
+        <Route exact path="/siteinfo">
+          {({ match }) => (
+            <CSSTransition in={match != null} classNames="fade" unmountOnExit timeout={1000}>
+              <SiteInfoPage />
+            </CSSTransition>
+          )}
+        </Route>
+        <Redirect exact from="/" to="/home" />
       </div>
     </div>
   )
