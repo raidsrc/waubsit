0b8a85353fa9f999a4edaca72e9d3d4d6a3d9031
Ray Louis <x1337c@gmail.com>
2022-02-16 22:06:24 -0800

    max-width'd the navbar

--- src/Pages/LinksPage.jsx
+++ src/Pages/LinksPage.jsx
@@ -119,7 +119,7 @@ function LinkBlock(props) {
         <button className="underline underline-offset-2 text-left z-30 relative" onClick={() => { setOpenUp(prev => !prev); setRotated(prev => !prev) }}>
           <div className="flex justify-start items-center">
             <animated.div style={caretRotationProps}>
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-right-fill" viewBox="0 0 16 16">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-caret-right-fill" viewBox="0 0 16 16">
                 <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z" />
               </svg>
             </animated.div>
